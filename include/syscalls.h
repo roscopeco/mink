14338bd032b41d172ea359fd2e3408e5d8133130
Ross Bamford <roscopeco@gmail.com>
2018-10-21 11:51:08 +0100

    First cut syscalls.h

---
+++ include/syscalls.h
@@ -0,0 +1,13 @@
+/* kmalloc.h - Kmalloc/kfree for Mink kernel.
+ *
+ * Copyright (c)2018 Ross Bamford. See LICENSE for details.
+ */
+#ifndef _MINK_SYSCALLS_H
+#define _MINK_SYSCALLS_H
+
+#define SYSCALL_INTR 0x80
+
+#define SYSCALL_SYS_WRITE 0x01
+
+
+#endif
